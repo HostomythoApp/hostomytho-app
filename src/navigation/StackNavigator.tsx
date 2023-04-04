1341ac60259968343841bc8731f90d09762e6abb
Beremy <bertrand.remy@inria.fr>
2023-04-04 15:26:54 +0200

    Add radiobutton

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -3,7 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MainScreen from "screens/MainScreen";
 import ProfileScreen from "screens/ProfileScreen";
 import HomeScreen from "screens/HomeScreen";
-import {TouchableOpacity} from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import LoginScreen from "../screens/LoginScreen";
 import SignIn from "../screens/SignIn";
 import PlausibilityGameScreen from "../screens/PlausibilityGameScreen";
@@ -23,13 +23,13 @@ const StackNavigator = ({ }) => {
         })}
       >
       </Stack.Group>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Connected" component={ProfileScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="Main" component={MainScreen} />
-      <Stack.Screen name={"PausibilityGame"} component={PlausibilityGameScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Connected" component={ProfileScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Main" component={MainScreen} />
+      <Stack.Screen name="PausibilityGame" component={PlausibilityGameScreen} />
     </Stack.Navigator>
   );
 };
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,15 +1,26 @@
-import React, {useState} from "react";
-import {Button, Text, TextInput, TouchableOpacity, View} from "react-native";
-import {useTailwind} from "tailwind-rn";
+import React, { useState } from "react";
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useTailwind } from "tailwind-rn";
 import MainInput from "../components/MainInput";
 import FunctionButton from "../components/FunctionButton";
+import RadioButton from 'components/RadioButton';
 
 
-function RadioButton(props: { value: string }) {
-    return null;
-}
 
-const SignIn = ({}) => {
+// function RadioButton(props: { value: string }) {
+//     return null;
+// }
+
+const SignIn = ({ }) => {
+
+    const [selectedValue, setSelectedValue] = useState('option1');
+
+    const options = [
+        { label: 'Option 1', value: 'option1' },
+        { label: 'Option 2', value: 'option2' },
+        { label: 'Option 3', value: 'option3' },
+    ];
+
     const tw = useTailwind();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -22,10 +33,10 @@ const SignIn = ({}) => {
         } else {
             if (password !== password2) {
                 alert('Mots de passe différents');
-            }else {
+            } else {
                 if (password.length < 6) {
                     alert('Mot de passe trop court');
-                }else {
+                } else {
                     alert(`Nom : ${username}\nPassword : ${password}`);
                 }
             }
@@ -36,7 +47,7 @@ const SignIn = ({}) => {
         const detectiveNames = ["Sherbloque", "Hermule", "Thilip", "Fancy", "Colombe", "Adrianne",
             "Maison", "Gris", "Chapardé", "Banor", "Masloy", "Médith", "Feur",];
 
-        const keywords = ["le vaillant", "l'intrépide", "l'observeur", "le minutieux", "le pro", "le fou","le poète"];
+        const keywords = ["le vaillant", "l'intrépide", "l'observeur", "le minutieux", "le pro", "le fou", "le poète"];
 
         const name = detectiveNames[Math.floor(Math.random() * detectiveNames.length)];
 
@@ -49,17 +60,28 @@ const SignIn = ({}) => {
     return (
         <View style={tw("flex-1 justify-center items-center")}>
 
-            <MainInput text={"Pseudo"} value={username} setter={setUsername} hide={false}/>
+            <MainInput text={"Pseudo"} value={username} setter={setUsername} hide={false} />
 
-            <FunctionButton text={"Générer un pseudo"} func={generatePseudo}/>
+            <FunctionButton text={"Générer un pseudo"} func={generatePseudo} />
 
-            <MainInput text={"Mot de passe"} value={password} setter={setPassword} hide={true}/>
+            <MainInput text={"Mot de passe"} value={password} setter={setPassword} hide={true} />
 
-            <MainInput text={"Retaper votre mot de passe"} value={password2} setter={setPassword2} hide={true}/>
+            <MainInput text={"Retaper votre mot de passe"} value={password2} setter={setPassword2} hide={true} />
 
-            <MainInput text={"email (facultatif)"} value={email} setter={setEmail} hide={false}/>
+            <MainInput text={"email (facultatif)"} value={email} setter={setEmail} hide={false} />
 
-            <FunctionButton text={"Inscription"} func={submit}/>
+            <View style={tw('p-4')}>
+                {options.map((option) => (
+                    <RadioButton
+                        key={option.value}
+                        label={option.label}
+                        selected={selectedValue === option.value}
+                        onPress={() => setSelectedValue(option.value)}
+                    />
+                ))}
+            </View>
+
+            <FunctionButton text={"Inscription"} func={submit} />
         </View>
     );
 };
