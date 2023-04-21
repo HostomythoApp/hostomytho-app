72855b58d3e8db3ba1406a8a864ed51832393c80
hostomytho <bertrand.remy@inria.fr>
2023-04-21 14:56:46 +0200

    Enhancement temporalEntityScreen

--- src/screens/TemporalEntityScreen.tsx
+++ src/screens/TemporalEntityScreen.tsx
@@ -8,11 +8,22 @@ import {
 } from "react-native";
 import { useTailwind } from "tailwind-rn";
 import data from "data/fakeUserData.js";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const colors = [
+  "bg-yellow-300",
+  "bg-green-300",
+  "bg-blue-300",
+  "bg-indigo-300",
+  "bg-pink-300",
+];
 
 const TemporalEntityScreen = ({ }) => {
   const tw = useTailwind();
   const [sentences, setSentences] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [temporalEntities, setTemporalEntities] = useState([]);
+  const [colorIndex, setColorIndex] = useState(0);
 
   function shuffleArray(array) {
     const newArr = [...array];
@@ -26,7 +37,7 @@ const TemporalEntityScreen = ({ }) => {
   useEffect(() => {
     const shuffledSentences = shuffleArray(data.sentences);
     setSentences(shuffledSentences.slice(0, 10).map((sentence) => {
-      const words = sentence.content.split(' ').map((word) => ({ text: word, isSelected: false }));
+      const words = sentence.content.split(' ').map((word) => ({ text: word, isSelected: false, entityId: null }));
       return { ...sentence, content: words };
     }));
   }, []);
@@ -36,7 +47,7 @@ const TemporalEntityScreen = ({ }) => {
       if (idx === sentenceIndex) {
         const newWords = sentence.content.map((word, idx) => {
           if (idx === wordIndex) {
-            return { ...word, isSelected: !word.isSelected };
+            return { ...word, isSelected: !word.isSelected, entityId: word.isSelected ? null : temporalEntities.length };
           }
           return word;
         });
@@ -48,15 +59,42 @@ const TemporalEntityScreen = ({ }) => {
     setSentences(newSentences);
   };
 
-  const renderSentence = (sentence, index) => {
-    if (!sentence) return null;
+  const addTemporalEntity = () => {
+    const selectedWords = sentences[currentIndex].content.filter(word => word.isSelected && word.entityId === temporalEntities.length);
+    const entityText = selectedWords.map(word => word.text).join(' ');
 
+    if (entityText) {
+      setTemporalEntities([...temporalEntities, { id: temporalEntities.length, text: entityText }]);
+      setColorIndex((colorIndex + 1) % colors.length);
+    }
+  };
+
+  const removeTemporalEntity = (entityId) => {
+    setTemporalEntities(temporalEntities.filter(entity => entity.id !== entityId));
+
+    const newSentences = sentences.map(sentence => {
+      const newWords = sentence.content.map(word => {
+        if (word.entityId === entityId) {
+          return { ...word, isSelected: false, entityId: null };
+        }
+        return word;
+      });
+      return { ...sentence, content: newWords };
+    });
+
+    setSentences(newSentences);
+  };
+
+  const renderSentence = (sentence, index) => {
+    if (typeof sentence === "undefined") {
+      return null;
+    }
     return (
       <View
         style={[
-          tw("bg-[#FFFEE0] rounded-xl justify-center"),
+          tw("bg-[#FFFEE0] rounded-xl justify-center mx-2"),
           {
-            minHeight: 400,
+            minHeight: 300,
             shadowColor: "#000",
             shadowOffset: { width: 0, height: 2 },
             shadowOpacity: 0.25,
@@ -65,16 +103,23 @@ const TemporalEntityScreen = ({ }) => {
           },
         ]}
       >
-        <View style={tw("flex-row flex-wrap")}>
+        <View style={tw("flex-row flex-wrap mb-2 m-7")}>
           {sentence.content.map((word, idx) => (
             <TouchableOpacity
               key={idx}
               onPress={() => onWordPress(idx, index)}
               style={tw(
-                `m-1 ${word.isSelected ? "bg-yellow-300" : "bg-transparent"}`
+                `m-1 ${word.isSelected ? colors[word.entityId % colors.length] : "bg-transparent"}`
               )}
             >
-              <Text>{word.text}</Text>
+              <Text style={[
+                tw("text-2xl"),
+                {
+                  fontFamily: "HandleeRegular",
+                },
+              ]}
+
+              >{word.text}</Text>
             </TouchableOpacity>
           ))}
         </View>
@@ -82,9 +127,19 @@ const TemporalEntityScreen = ({ }) => {
     );
   };
 
+  const renderTemporalEntity = (entity) => (
+    <View key={entity.id} style={tw("flex-row items-center")}>
+      <Text style={tw("mr-2")}>{entity.text}</Text>
+      <TouchableOpacity onPress={() => removeTemporalEntity(entity.id)}>
+        <Text style={tw("text-red-500")}>×</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   const onNextCard = () => {
     if (currentIndex < sentences.length - 1) {
       setCurrentIndex(currentIndex + 1);
+      setTemporalEntities([]); // Réinitialiser le récapitulatif des entités
     }
   };
 
@@ -93,12 +148,23 @@ const TemporalEntityScreen = ({ }) => {
       <View style={tw("flex-1 justify-center items-center")}>
         {renderSentence(sentences[currentIndex], currentIndex)}
       </View>
-      <TouchableOpacity
-        style={tw("bg-blue-500 py-2 px-4 rounded-lg self-center mb-4")}
-        onPress={onNextCard}
-      >
-        <Text style={tw("text-white")}>Carte suivante</Text>
-      </TouchableOpacity>
+      <View style={tw("mb-4")}>
+        {temporalEntities.map(entity => renderTemporalEntity(entity))}
+      </View>
+      <View style={tw("flex-row justify-between items-center mb-4 mx-2")}>
+        <TouchableOpacity
+          style={tw("bg-blue-500 py-2 px-4 rounded-lg")}
+          onPress={addTemporalEntity}
+        >
+          <Text style={tw("text-white")}>Nouvelle entité</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw("bg-blue-500 py-2 px-4 rounded-lg")}
+          onPress={onNextCard}
+        >
+          <Icon name="arrow-right" size={20} color="#FFFFFF" />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
