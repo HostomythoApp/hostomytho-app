ab128f6d7f55ec6228e8908735bba282636011f4
hostomytho <bertrand.remy@inria.fr>
2023-05-04 13:49:57 +0200

    Graphic changes on profile page

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -3,15 +3,15 @@ import { TouchableOpacity, Text, View, ScrollView } from 'react-native';
 import { useTailwind } from 'tailwind-rn';
 import withAuth from 'services/auth/withAuth';
 import { useUser } from '../services/auth/UserContext';
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5, MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
 
 const ProfileScreen = (props) => {
     const tw = useTailwind();
     const { user } = useUser();
 
     const achievements = [
-        { id: 1, title: 'Premier texte validé' },
-        { id: 2, title: '10 annotations créées' },
+        { id: 1, title: 'Premier texte validé', picto: <Entypo name="medal" size={24} color="gold" /> },
+        { id: 2, title: '10 annotations créées', picto: <MaterialCommunityIcons name="rewind-10" size={24} color="mediumseagreen" /> },
     ];
 
     const stats = [
@@ -33,93 +33,100 @@ const ProfileScreen = (props) => {
 
     return (
         <ScrollView style={tw('flex-1 p-4')}>
-            <Text style={tw('text-2xl font-bold mb-2')}>{user?.username}</Text>
+            <View style={tw('mx-auto')} maxWidth={840}>
+                <Text style={tw('text-2xl font-bold mb-2 text-center')}>{user?.username}</Text>
 
-            <Text style={tw('text-lg mb-4')}>Points: {user?.points}</Text>
+                <Text style={tw('text-lg mb-4 text-center')}>Points: {user?.points}</Text>
 
-
-            <View style={tw('w-full mt-4')}>
-                <Text style={tw('text-xl font-bold mb-2')}>
-                    Progression avant la prochaine récompense
-                </Text>
-                <View style={tw('bg-gray-300 h-4 rounded')}>
-                    <View
-                        style={[
-                            tw('bg-blue-500 h-full rounded-l'),
-                            { width: `${pointsPercentage}%` },
-                        ]}
-                    ></View>
-                </View>
-                <Text style={tw('text-xs mt-1')}>
-                    {currentPoints} / {nextRewardPoints} points
-                </Text>
-            </View>
-
-            <View style={tw('')}>
-                <Text style={tw('text-xl font-bold mt-4 mb-2')}>Classement</Text>
-                {ranking.map((rank) => (
-                    <View
-                        key={rank.position}
-                        style={[
-                            tw('p-2 flex-row items-center justify-between'),
-                            rank.position === 32 && tw('bg-blue-100'),
-                        ]}
-                    >
-                        <Text>
-                            {rank.position}. {rank.username}
-                        </Text>
-                        <Text>{rank.points} points</Text>
-                    </View>
-                ))}
-                <TouchableOpacity onPress={() => navigation.navigate('Ranking')}>
-                    <Text style={tw('text-blue-500 mt-1')}>Voir le classement complet</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View style={tw('flex-row justify-between my-6')}>
-                <View style={tw('flex-1 mr-2')}>
-                    <Text style={tw('text-xl font-bold mb-2')}>Hauts faits</Text>
-                    {achievements.map((achievement) => (
-                        <Text key={achievement.id}>{achievement.title}</Text>
-                    ))}
-                    <TouchableOpacity onPress={() => navigation.navigate('Achievements')}>
-                        <Text style={tw('text-blue-500 mt-1')}>Tout afficher</Text>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={tw('flex-1 ml-2')}>
-                    <Text style={tw('text-xl font-bold mb-2')}>Statistiques</Text>
-                    {stats.map((stat) => (
-                        <Text key={stat.id}>
-                            {stat.title}: {stat.count}
-                        </Text>
-                    ))}
-                    <TouchableOpacity onPress={() => navigation.navigate('Stats')}>
-                        <Text style={tw('text-blue-500 mt-1')}>Tout afficher</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-
-            <Text style={tw(' text-base mb-1 my-2')}>
-                {user?.email ? user.email : "Vous n'avez pas renseigné d'email"}
-            </Text>
-
-            <View style={tw('flex-row mt-4')}>
-                <TouchableOpacity
-                    style={tw('bg-blue-500 px-4 py-2 rounded mr-2')}
-                    onPress={() => navigation.navigate('Contacts')}
-                >
-                    <Text style={tw('text-white')}>
-                        <FontAwesome5 name='user-friends' size={24} color='white' />
-                        Mes contacts
+                <View style={tw('w-full mt-4')}>
+                    <Text style={tw('text-lg font-bold mb-2')}>
+                        Progression avant la prochaine récompense
                     </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={tw('bg-green-500 px-4 py-2 rounded ml-2')}
-                    onPress={() => navigation.navigate('Refer')}
-                >
-                    <Text style={tw('text-white')}>Parrainer quelqu'un</Text>
-                </TouchableOpacity>
+                    <View style={tw('bg-gray-300 h-4 rounded')}>
+                        <View
+                            style={[
+                                tw('bg-blue-500 h-full rounded-l'),
+                                { width: `${pointsPercentage}%` },
+                            ]}
+                        ></View>
+                    </View>
+                    <Text style={tw('text-xs mt-1')}>
+                        {currentPoints} / {nextRewardPoints} points
+                    </Text>
+                </View>
+
+                <View style={tw(' bg-white mt-8 mb-2 py-2')}>
+                    <Text style={tw('text-xl font-bold mb-2 ')}>Classement</Text>
+                    {ranking.map((rank) => (
+                        <View
+                            key={rank.position}
+                            style={[
+                                tw('p-2 flex-row items-center justify-between '),
+                                rank.position === 32 && tw('bg-blue-100'),
+                            ]}
+                        >
+                            <Text>
+                                {rank.position}. {rank.username}
+                            </Text>
+                            <Text>{rank.points} points</Text>
+                        </View>
+                    ))}
+                    <TouchableOpacity onPress={() => navigation.navigate('Ranking')}>
+                        <Text style={tw('text-blue-500 mt-1')}>Voir le classement complet</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={tw('flex-row justify-between my-6')}>
+                    <View style={tw('flex-1 mr-2')}>
+                        <Text style={tw('text-xl font-bold mb-2')}>Hauts faits</Text>
+                        {achievements.map((achievement) => (
+                            <Text key={achievement.id}>{achievement.picto}  {achievement.title}</Text>
+                        ))}
+                        <TouchableOpacity onPress={() => navigation.navigate('Achievements')}>
+                            <Text style={tw('text-blue-500 mt-1')}>Tout afficher</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={tw('border-r border-gray-300 h-full mx-2')} />
+                    <View style={tw('flex-1 ml-2')}>
+                        <Text style={tw('text-xl font-bold mb-2')}>Statistiques</Text>
+                        {stats.map((stat) => (
+                            <Text key={stat.id}>
+                                {stat.title}: {stat.count}
+                            </Text>
+                        ))}
+                        <TouchableOpacity onPress={() => navigation.navigate('Stats')}>
+                            <Text style={tw('text-blue-500 mt-1')}>Tout afficher</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <Text style={tw(' text-base mb-1 my-2')}>
+                    {user?.email ? user.email : "Vous n'avez pas renseigné d'email"}
+                </Text>
+
+                <Text style={tw('text-base mb-2')}>
+                    {user?.status ? `Vous êtes ${user.status}` : "Aucun statut renseigné"}
+                </Text>
+
+                <View style={tw('flex-row items-center justify-center mt-4')}>
+                    <TouchableOpacity
+                        style={tw('bg-blue-500 px-4 py-2 rounded mr-2')}
+                        onPress={() => navigation.navigate('Contacts')}
+                    >
+                        <Text style={tw('text-white')}>
+                            <FontAwesome5 style={tw('mr-2')} name='user-friends' size={24} color='white' />
+                            Mes contacts
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={tw('bg-green-500 px-4 py-2 rounded ml-2')}
+                        onPress={() => navigation.navigate('Referral')}
+                    >
+                        <Text style={tw('text-white')}>
+                            Parrainer quelqu'un
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </ScrollView>
     );
