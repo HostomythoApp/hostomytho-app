8417aaedc827b1f237b760e84e9c0206998c7d5f
hostomytho <bertrand.remy@inria.fr>
2023-05-05 17:12:43 +0200

    Begin of achievements

---
+++ src/screens/AchievementsScreen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { useTailwind } from "tailwind-rn";
+
+
+const AchievementsScreen = ({ }) => {
+    const tw = useTailwind();
+
+    return (
+        <View style={tw("flex-1 justify-center items-center")}>
+            <Text style={tw(' text-xl')}>Hauts faits</Text>
+        </View>);
+};
+
+export default AchievementsScreen;
